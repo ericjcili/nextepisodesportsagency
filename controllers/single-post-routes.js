87b1b92836b65936fcbe49875d5874059c6fbfba
ericjcili <ericjcili@gmai.com>
2022-07-28 18:12:09 -0400

    before profile changes

--- controllers/single-post-routes.js
+++ controllers/single-post-routes.js
@@ -66,4 +66,73 @@ router.get('/', withAuth, (req, res) => {
           });
       });
 
+      router.get('/', (req, res) => {
+        Post.findAll({
+            attributes: [
+                'id',
+                'post_desc',
+                'post_text',
+                'title',
+                'created_at',
+                'tags'
+              ],
+            order: [[ 'created_at', 'DESC']],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+          const posts = dbPostData.map(post => post.get({ plain: true }));
+          res.render('our-services', {
+            posts,
+            loggedIn: req.session.loggedIn
+          });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+        
+      });
+      
+      router.get('/post/:id', (req, res) => {
+          Post.findOne({
+            where: {
+              id: req.params.id
+            },
+            attributes: [
+              'id',
+              'post_desc',
+              'post_text',
+              'title',
+              'created_at',
+              'tags'
+            ],
+            include: [
+              {
+                model: User,
+                attributes: ['username']
+              }
+            ]
+          })
+            .then(dbPostData => {
+              if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+              }
+              const post = dbPostData.get({ plain: true });
+              res.render('single-post', {
+                  post,
+                  loggedIn: req.session.loggedIn
+                });
+            })
+            .catch(err => {
+              console.log(err);
+              res.status(500).json(err);
+            });
+        });
+
       module.exports = router;
